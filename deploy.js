cab44296d4f64fd1518aa1f746487f140070b18f
Noel Forte <noel@forte.is>
2022-05-10 21:28:25 -0400

    added a test workflow

--- deploy.js
+++ deploy.js
@@ -1,8 +1,28 @@
 #!/bin/env node
 
 const child_process = require("node:child_process");
-const args = process.argv.slice(2).join(" ");
+const args = process.argv[2]
+	.split(",")
+	.map(element => {
+		if (element.includes("production=")) {
+			return element === "production=true" ? ["--prod"] : [];
+		} else {
+			const command = element.split("=");
+			return [`--${command[0]}`, command[1]];
+		}
+	})
+	.flat();
 
-child_process.exec(`./node_modules/.bin/netlify deploy --json ${args}`, (error, stdout, stderr) => {
-	console.log(output);
-});
+child_process.execFile(
+	"./node_modules/.bin/netlify",
+	["deploy", "--json", ...args],
+	(error, stdout, stderr) => {
+		if (error) {
+			console.log(error);
+		} else if (stdout) {
+			console.log(stdout);
+		} else {
+			console.log(stderr);
+		}
+	}
+);
